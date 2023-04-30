198e80e95433db5a99fed043bc1b3da9f11e1163
scziskh <muzychukserhii@gmail.com>
2023-04-30 20:41:22 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { createGlobalStyles } from "solid-styled-components";
+
+const App = () => {
+  return (
+    <>
+      <GlobalStyle />
+    </>
+  );
+};
+
+export default App;
+
+const GlobalStyle = createGlobalStyles``;
